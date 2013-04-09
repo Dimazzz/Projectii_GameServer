6672f810f3326e643e7df53faed292363d809aea
Yau Ref <yau.ref@gmail.com>
2013-04-09 11:05:42 +0400

    Created class to communicate with confirmed clients

---
+++ src/org/projii/serverside/gs/interaction/OutLayer.java
@@ -0,0 +1,63 @@
+package org.projii.serverside.gs.interaction;
+
+import org.jboss.netty.channel.Channel;
+import org.projii.commons.net.InteractionMessage;
+
+import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+
+public class OutLayer {
+
+    private final ExecutorService workers;
+    private Channel coordinationServerChannel;
+    private HashMap<Integer, Channel> clientChannels;
+
+    public OutLayer(ExecutorService workers) {
+        this.workers = workers;
+    }
+
+    public void setCS(Channel channel) {
+        coordinationServerChannel = channel;
+    }
+
+    public void addClient(Channel channel, int sessionId) {
+        if (!clientChannels.containsKey(sessionId)) {
+            clientChannels.put(sessionId, channel);
+        }
+    }
+
+    public void send(final InteractionMessage interactionMessage) {
+        workers.execute(new Runnable() {
+            @Override
+            public void run() {
+                for (Channel channel : clientChannels.values()) {
+                    channel.write(interactionMessage);
+                }
+            }
+        });
+    }
+
+    public void send(final InteractionMessage interactionMessage, int sessionId) {
+        final Channel channel = clientChannels.get(sessionId);
+        if (channel != null) {
+            workers.execute(new Runnable() {
+                @Override
+                public void run() {
+                    channel.write(interactionMessage);
+                }
+            });
+        }
+    }
+
+    public void sendToCS(final InteractionMessage interactionMessage) {
+        if (coordinationServerChannel != null) {
+            workers.execute(new Runnable() {
+                @Override
+                public void run() {
+                    coordinationServerChannel.write(interactionMessage);
+                }
+            });
+        }
+    }
+
+}
