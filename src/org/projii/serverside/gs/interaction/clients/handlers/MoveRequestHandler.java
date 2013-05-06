868b8854e6a8c2b929bf935ef4fd8bceff21a21d
Yau Ref <yau.ref@gmail.com>
2013-05-06 10:12:37 +0400

    Client move and fire requests

---
+++ src/org/projii/serverside/gs/interaction/clients/handlers/MoveRequestHandler.java
@@ -0,0 +1,14 @@
+package org.projii.serverside.gs.interaction.clients.handlers;
+
+import org.jboss.netty.channel.Channel;
+import org.projii.commons.net.InteractionMessage;
+import org.projii.serverside.commons.RequestHandler;
+
+public class MoveRequestHandler  implements RequestHandler {
+
+    @Override
+    public void handle(InteractionMessage interactionMessage, Channel channel) {
+
+    }
+
+}
---
+++ src/org/projii/serverside/gs/interaction/clients/requests/FireRequest.java
@@ -0,0 +1,10 @@
+package org.projii.serverside.gs.interaction.clients.requests;
+
+import org.projii.commons.net.InteractionMessage;
+
+public class FireRequest implements InteractionMessage {
+    @Override
+    public int getType() {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
